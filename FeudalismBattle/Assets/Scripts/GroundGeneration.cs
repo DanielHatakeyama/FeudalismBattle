9d12bd72911bfdde1b9ebe4c733a2f00eb4b1c42
DanielHatakeyama <hatacoyama@gmail.com>
2023-01-17 00:35:32 -0700

    basic perlin noise water it is there but it wok=nt be easty to export and work unless it is mee sorry about that

--- FeudalismBattle/Assets/Scripts/GroundGeneration.cs
+++ FeudalismBattle/Assets/Scripts/GroundGeneration.cs
@@ -1,18 +1,106 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class GroundGeneration : MonoBehaviour
 {
+
+    [SerializeField] 
+    private Vector2 mapSize;
+
+    public Tilemap groundTilemap;
+    public Tilemap waterTilemap;
+
+    public RuleTile waterRuleTile;
+    public Tile grassTile;
+
+    public float noiseScale = 0.1f;
+
+
     // Start is called before the first frame update
     public void Start()
     {
+
+        // Get children tilemaps
+        groundTilemap = transform.Find("Ground").GetComponent<Tilemap>();
+        waterTilemap  = transform.Find("Water").GetComponent<Tilemap>();
+
+        Debug.Log(mapSize);
+
+        // Set the entire background to grass, just for now
+        for (int x = 0; x < mapSize.x; x++) {
+            for (int y = 0; y < mapSize.y; y++) {
+                groundTilemap.SetTile(new Vector3Int(x, y, 0), grassTile);
+            }
+        }
+
+        for (int x = 0; x < mapSize.x; x++)
+        {
+            for (int y = 0; y < mapSize.y; y++)
+            {
+                float noise = Mathf.PerlinNoise((x + 0.5f) * noiseScale, (y + 0.5f) * noiseScale);
+
+                if (noise > 0.5f)
+                {
+                    waterTilemap.SetTile(new Vector3Int(x, y, 0), waterRuleTile);
+                }
+            }
+        }
+
+        // waterTilemap.SetTile(new Vector3Int(0, 0, 0), waterRuleTile);
+        // waterTilemap.SetTile(new Vector3Int(1, 0, 0), waterRuleTile);
+        // waterTilemap.SetTile(new Vector3Int(0, 1, 0), waterRuleTile);
+
+
         
+        // mapHeight = mapSize.y;
+        // mapWidth = mapSize.x;
+
+        // Terrain perlin noise generation
+        // for (int x = 0; x < mapWidth; x++) {
+        //     for (int y = 0; y < mapHeight; y++) {
+
+        //     }
+        // }
+
+
+        // Create a 2D array to store the terrain data
+        // float[,] terrainData = new float[mapWidth, mapHeight];
+
+        // Fill the terrainData array with random noise values
+        // for (int x = 0; x < mapWidth; x++)
+        // {
+        //     for (int y = 0; y < mapHeight; y++)
+        //     {
+        //         terrainData[x, y] = Mathf.PerlinNoise((float)x / mapWidth * scale, (float)y / mapHeight * scale);
+        //     }
+        // }
+
+        // // Iterate through the terrainData array and instantiate the appropriate tile
+        // for (int x = 0; x < mapWidth; x++)
+        // {
+        //     for (int y = 0; y < mapHeight; y++)
+        //     {
+        //         // Set a threshold for water (e.g. 0.2)
+        //         if (terrainData[x, y] < 0.2)
+        //         {
+        //             // Instantiate a water tile
+        //             Instantiate(waterTile, new Vector3(x, y, 0), Quaternion.identity);
+        //         }
+        //         else
+        //         {
+        //             // Instantiate a grass tile
+        //             Instantiate(grassTile, new Vector3(x, y, 0), Quaternion.identity);
+        //         }
+        //     }
+        // }
+
     }
 
-    // Update is called once per frame
+    // Update is called once per frame  
     public void Update()
     {
-        
+
     }
 }
---
+++ FeudalismBattle/Assets/Scripts/TempCamMovement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempCamMovement : MonoBehaviour
+{
+   public float speed = 10f;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += new Vector3(0, speed * Time.deltaTime, 0);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position -= new Vector3(0, speed * Time.deltaTime, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position -= new Vector3(speed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+        }
+    }
+}
