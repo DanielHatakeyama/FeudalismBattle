acab8baa43e55adc746c93dffb91d1b41052ac6d
DanielHatakeyama <41268067+DanielHatakeyama@users.noreply.github.com>
2023-01-17 18:06:56 -0700

    Added camera clamping in scene

---
+++ FeudalismBattle/Assets/Scripts/CameraBehavior.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehavior : MonoBehaviour
+{
+    [SerializeField] private GroundGeneration groundGeneration;
+
+    private Vector2 maxBounds;
+    private Vector2 minBounds;
+
+    private Camera camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = GetComponent<Camera>();
+
+        minBounds = new Vector2(camera.orthographicSize * camera.aspect, camera.orthographicSize);
+        maxBounds = groundGeneration.mapSize - new Vector2(camera.orthographicSize * camera.aspect, camera.orthographicSize);
+
+        
+    }
+
+    // Update is called once per frame
+    private void LateUpdate()
+    {
+        Vector3 cameraPosition = Camera.main.transform.position;
+        cameraPosition.x = Mathf.Clamp(cameraPosition.x, minBounds.x, maxBounds.x);
+        cameraPosition.y = Mathf.Clamp(cameraPosition.y, minBounds.y, maxBounds.y);
+        Camera.main.transform.position = cameraPosition;
+    }
+}
